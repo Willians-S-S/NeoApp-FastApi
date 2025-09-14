a63128c42dcee5d6a1c9f9be2323fc22242c30d8
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-14 18:55:26 -0300

    feat: adicionar UserModel com mapeamento do SQLAlchemy

---
+++ src/db/models/user_models.py
@@ -0,0 +1,25 @@
+from datetime import date, datetime
+from sqlalchemy import String, Date, DateTime, Table
+from sqlalchemy.orm import Mapped, registry, mapped_column
+from sqlalchemy.sql import func
+
+table_registry = registry()
+
+@table_registry.mapped_as_dataclass
+class UserModel:
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True)  
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    birthday: Mapped[date] = mapped_column(Date, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20))
+    cpf: Mapped[str] = mapped_column(String(11), nullable=False, unique=True)
+
+    creat_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    update_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
